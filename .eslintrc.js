98b065d841bfacb05c9aa9972fa27f1d77e54a47
heyjo <heyjo@notvalid.com>
2019-08-06 17:39:41 -0300

    adding routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es6: true
   },
+  plugins: ['@typescript-eslint'],
   extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
     'standard'
   ],
   globals: {
