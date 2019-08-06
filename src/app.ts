9dcbab2acb2cbec0add5b680fde44202083dae21
heyjo <heyjo@notvalid.com>
2019-08-06 17:53:08 -0300

    extending interface

--- src/app.ts
+++ src/app.ts
@@ -27,9 +27,7 @@ class App {
     }
 
     private routes (): void {
-      this.express.get('/', (req, res) => {
-        return res.send('Hello World')
-      })
+      this.express.use(routes)
     }
 }
 
